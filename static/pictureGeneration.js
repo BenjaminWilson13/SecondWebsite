aa5cbb4675ca2e53526c43ac851e811d99f2a438
Benjamin Wilson <benjamin.blake.wilson13@gmail.com>
2023-04-13 21:34:13 -0500

    cleaning up after a horribly failed experiment

--- static/pictureGeneration.js
+++ static/pictureGeneration.js
@@ -5,49 +5,49 @@ function getRandomIntInclusive(min, max) {
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const randomNumber = getRandomIntInclusive(0, 10000); 
-    document.cookie = `sessionId=${randomNumber}`; 
-    const button = document.querySelector('#submit'); 
+    const randomNumber = getRandomIntInclusive(0, 10000);
+    document.cookie = `sessionId=${randomNumber}`;
+    const button = document.querySelector('#submit');
 
 
     button.addEventListener('click', async (event) => {
-        event.preventDefault(); 
-        const promptBox = document.querySelector('#prompt'); 
-        const legend = document.querySelector('legend'); 
+        event.preventDefault();
+        const promptBox = document.querySelector('#prompt');
+        const legend = document.querySelector('legend');
         if (!promptBox.value) {
-            legend.style.color = 'red'; 
-            return legend.innerText = 'Gotta enter some text!'; 
+            legend.style.color = 'red';
+            return legend.innerText = 'Gotta enter some text!';
         }
-        const pictureBox = document.querySelector('#pictureBox'); 
-        const promptValue = promptBox.value; 
-        legend.style.color = 'green'; 
-        legend.innerText = 'Generating! One moment please!'; 
-        promptBox.value = ''; 
-        const img = document.createElement('img'); 
-        img.src = "data:image/png;base64,"; 
-        const pTag = document.createElement('h3'); 
-        pTag.innerText = `Your Prompt: ${promptValue}`; 
-        let status = await fetch(`/generateImage/${promptValue}`); 
-        status = await status.json(); 
+        const pictureBox = document.querySelector('#pictureBox');
+        const promptValue = promptBox.value;
+        legend.style.color = 'green';
+        legend.innerText = 'Generating! One moment please!';
+        promptBox.value = '';
+        const img = document.createElement('img');
+        img.src = "data:image/png;base64,";
+        const pTag = document.createElement('h3');
+        pTag.innerText = `Your Prompt: ${promptValue}`;
+        let status = await fetch(`/generateImage/${promptValue}`);
+        status = await status.json();
         document.cookie = `task=${status.task}`;
         legend.innerText += `   Your place in queue is: ${status.queue}`
-        
+
         //getting the picture now
         let picture = await fetch(`/getImage`);
-        picture = await picture.text(); 
-        img.src += picture; 
-        img.style.marginBottom = '25px'; 
-        pictureBox.appendChild(img); 
-        pictureBox.appendChild(pTag); 
+        picture = await picture.text();
+        img.src += picture;
+        img.style.marginBottom = '25px';
+        pictureBox.appendChild(img);
+        pictureBox.appendChild(pTag);
         legend.innerText = 'Generate a picture!'
-        legend.style.color = 'white'; 
+        legend.style.color = 'white';
     })
 
-    const statusHeader = document.querySelector('#status'); 
-    let status = await fetch('/aiImageAPI.html/onlineStatus'); 
-    status = await status.text(); 
+    const statusHeader = document.querySelector('#status');
+    let status = await fetch('/aiImageAPI.html/onlineStatus');
+    status = await status.text();
     if (status === `Online`) {
-        statusHeader.innerText = 'The image generator is Online!'; 
-        statusHeader.style.color = 'green'; 
+        statusHeader.innerText = 'The image generator is Online!';
+        statusHeader.style.color = 'green';
     }
 })
